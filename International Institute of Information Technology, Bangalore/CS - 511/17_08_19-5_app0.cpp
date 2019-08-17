e1c5a8dba1222143fce4edc1e5313ee35e8c5916
Parth <parthendo@geu.ac.in>
2019-08-18 00:52:00 +0530

    Algorithms for the weekend

--- International Institute of Information Technology, Bangalore/CS - 511/17_08_19-5_app0.cpp
+++ International Institute of Information Technology, Bangalore/CS - 511/17_08_19-5_app0.cpp
@@ -1,35 +1,35 @@
 /*
  * Problem Statement: Given an array of integers, find i and j such that
- * 1. i < j
+ * 1. i+L <= j
  * 2. A[j]-A[i] is maximum
  * Approach: Brute Force
  * Space Complexity: O(1)
  * Time Complexity: O(n^2)
  *
- * Input File: file_17_08_19-4.txt
+ * Input File: file_17_08_19-5.txt
  * Output:
- * Maximum difference is 997043
+ * Maximum difference is 998034
  * Maximum difference is 990891
- * Maximum difference is 191411
+ * Maximum difference is 189346
  */
 #include <iostream>
 #include <cstdio>
 #include <climits>
-
+#include <cmath>
 using namespace std;
 
 int main(){
-  FILE *fp = fopen("file_17_08_19-4.txt","r");
+
+  FILE *fp = fopen("file_17_08_19-5.txt","r");
   int t;  fscanf(fp,"%d",&t);
   while(t--){
-    int n;  fscanf(fp,"%d",&n);
+    int n,l;  fscanf(fp,"%d %d",&n,&l);
     int arr[n+5];
     for(int i=0;i<n;i++) fscanf(fp,"%d",&arr[i]);
     int maxDiff = INT_MIN;
-    for(int i=0;i<n-1;i++){
-      for(int j=i+1;j<n;j++){
-        if(arr[j]-arr[i]>maxDiff)
-          maxDiff = arr[j]-arr[i];
+    for(int i=0;i<n;i++){
+      for(int j=l;j<n;j++){
+        maxDiff=max(arr[j]-arr[i],maxDiff);
       }
     }
     cout << "Maximum difference is " << maxDiff << endl;
--- International Institute of Information Technology, Bangalore/CS - 511/17_08_19-5_app1.cpp
+++ International Institute of Information Technology, Bangalore/CS - 511/17_08_19-5_app1.cpp
@@ -1,36 +1,36 @@
 /*
  * Problem Statement: Given an array of integers, find i and j such that
- * 1. i < j
+ * 1. i+L <= j
  * 2. A[j]-A[i] is maximum
- * Approach: Brute Force
+ * Approach: Greedy Algorithm
  * Space Complexity: O(1)
- * Time Complexity: O(n^2)
+ * Time Complexity: O(n)
  *
- * Input File: file_17_08_19-4.txt
+ * Input File: file_17_08_19-5.txt
  * Output:
- * Maximum difference is 997043
+ * Maximum difference is 998034
  * Maximum difference is 990891
- * Maximum difference is 191411
+ * Maximum difference is 189346
  */
 #include <iostream>
 #include <cstdio>
+#include <cmath>
 #include <climits>
-
 using namespace std;
 
 int main(){
-  FILE *fp = fopen("file_17_08_19-4.txt","r");
+  FILE *fp = fopen("file_17_08_19-5.txt","r");
   int t;  fscanf(fp,"%d",&t);
   while(t--){
-    int n;  fscanf(fp,"%d",&n);
+    int n,l;  fscanf(fp,"%d %d",&n,&l);
     int arr[n+5];
     for(int i=0;i<n;i++) fscanf(fp,"%d",&arr[i]);
     int maxDiff = INT_MIN;
-    for(int i=0;i<n-1;i++){
-      for(int j=i+1;j<n;j++){
-        if(arr[j]-arr[i]>maxDiff)
-          maxDiff = arr[j]-arr[i];
-      }
+    int i = 0;
+    for(int j=l;j<n;j++){
+      maxDiff = max(maxDiff,arr[j]-arr[i]);
+      if(arr[i]>arr[j])
+        i = j-l+1;
     }
     cout << "Maximum difference is " << maxDiff << endl;
   }
